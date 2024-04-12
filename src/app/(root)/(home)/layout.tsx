203bd45739465ec5037d13cb9dc2ec1b57788cea
Hassan Ali <hassan@turingtechnologies.org>
2024-04-12 10:15:49 +0500

    auth and public assets added

---
+++ src/app/(root)/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+}
---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+
+export default function Home() {
+  console.log("I am client side ---");
+  return <div>Home</div>;
+}
