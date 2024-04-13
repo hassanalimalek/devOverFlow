cb788a9548f7e381e4844c444d5a3cdefe30d472
Hassan Ali <hassan@turingtechnologies.org>
2024-04-13 09:12:43 +0500

    left and right sidebar added

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -3,5 +3,5 @@ import React from "react";
 
 export default function Home() {
   console.log("I am client side ---");
-  return <div>Home</div>;
+  return <div className="">Home</div>;
 }
---
+++ src/components/sidebar/rightSidebar.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import React from "react";
+import RenderTag from "../renderTag";
+
+function RightSidebar() {
+  return (
+    <div
+      className="
+  background-light900_dark200 light-border flex flex-col  border-2 border-red-500 p-6 pt-36 shadow-light-300 dark:shadow-none max-md:hidden md:w-[320px]"
+    >
+      <div className="mb-20">
+        <h2 className="h3-bold text-dark300_light900 ">Top Questions</h2>
+        <div className="mt-6 flex flex-col gap-[30px]">
+          <div className="flex gap-4">
+            <p className="body-medium text-dark500_light700">
+              Would it be appropriate to point out an error in another paper
+              during a referee report?
+            </p>
+            <Image
+              alt="chevron right"
+              width={20}
+              height={20}
+              src="assets/icons/chevron-right.svg"
+            />
+          </div>
+          <div className="flex gap-4">
+            <p className="body-medium text-dark500_light700">
+              Would it be appropriate to point out an error in another paper
+              during a referee report?
+            </p>
+            <Image
+              alt="chevron right"
+              width={20}
+              height={20}
+              src="assets/icons/chevron-right.svg"
+            />
+          </div>
+          <div className="flex gap-4">
+            <p className="body-medium text-dark500_light700">
+              Would it be appropriate to point out an error in another paper
+              during a referee report?
+            </p>
+            <Image
+              alt="chevron right"
+              width={20}
+              height={20}
+              src="assets/icons/chevron-right.svg"
+            />
+          </div>
+          <div className="flex gap-4">
+            <p className="body-medium text-dark500_light700">
+              Would it be appropriate to point out an error in another paper
+              during a referee report?
+            </p>
+            <Image
+              alt="chevron right"
+              width={20}
+              height={20}
+              src="assets/icons/chevron-right.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2 className="h3-bold text-dark300_light900 ">Popular Tags</h2>
+        <div className="mt-6 flex flex-col gap-[30px]">
+          <RenderTag
+            _id="1"
+            name="NEXT JS"
+            showCount={true}
+            totalQuestions={1000}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RightSidebar;
